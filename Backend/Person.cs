aa61298e9b58c587ea1a509b0473233ffe76ef92
przemyslaw.jopa <przemyslaw.jopa@gmail.com>
2018-10-30 20:22:34 +0100

    Add Person class.

---
+++ Backend/Person.cs
@@ -0,0 +1,35 @@
+﻿
+namespace Backend
+{
+    /// <summary>
+    /// Abstract representation of the <see cref="Person" /> class.
+    /// </summary>
+    abstract class Person
+    {
+        /// <summary>
+        /// Gets or sets the name of the <see cref="Person"/>.
+        /// </summary>
+        /// <value>
+        /// The name.
+        /// </value>
+        protected string name { get; private set; }
+        /// <summary>
+        /// Gets or sets the surname of the <see cref="Person"/>.
+        /// </summary>
+        /// <value>
+        /// The surname.
+        /// </value>
+        protected string surname { get; private set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Person"/> class.
+        /// </summary>
+        protected Person() { }
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Person"/> class.
+        /// </summary>
+        /// <param name="name">The name.</param>
+        /// <param name="surname">The surname.</param>
+        protected Person(string name, string surname) { this.name = name; this.surname = surname; }
+    }
+}
