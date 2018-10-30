925eda5376a55ad22b7d72e956f4e30ffc5d693a
przemyslaw.jopa <przemyslaw.jopa@gmail.com>
2018-10-30 20:23:23 +0100

    Add Referee class.

---
+++ Backend/Referee.cs
@@ -0,0 +1,36 @@
+﻿
+namespace Backend
+{
+    /// <summary>
+    /// Representation of the Referee class.
+    /// </summary>
+    /// <seealso cref="Backend.Person" />
+    class Referee : Person
+    {
+        /// <summary>
+        /// Gets the name of the <see cref="Referee"/>.
+        /// </summary>
+        /// <value>
+        /// The name of the <see cref="Referee"/>.
+        /// </value>
+        public string GetName => base.name;
+        /// <summary>
+        /// Gets the <see cref="Referee"/>'s surname.
+        /// </summary>
+        /// <value>
+        /// The <see cref="Referee"/>'s surname.
+        /// </value>
+        public string GetSurname => base.surname;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Referee"/> class.
+        /// </summary>
+        public Referee() { }
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Referee"/> class.
+        /// </summary>
+        /// <param name="name">The name.</param>
+        /// <param name="surname">The surname.</param>
+        public Referee(string name,string surname) : base(name, surname) { }
+    }
+}
