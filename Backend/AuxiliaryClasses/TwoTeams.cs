48330b46feec3cb197757b82301e5376f07db86f
przemyslaw.jopa <przemyslaw.jopa@gmail.com>
2018-11-06 22:41:51 +0100

    Add TwoTeams class.

---
+++ Backend/AuxiliaryClasses/TwoTeams.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Backend.AuxiliaryClasses
+{
+    /// <summary>
+    /// <see cref="Tuple"/> class that can hold two objects of <see cref="Team"/> type.
+    /// </summary>
+    /// <seealso cref="System.Tuple{Backend.Team, Backend.Team}" />
+    class TwoTeams : Tuple<Team, Team>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TwoTeams"/> class.
+        /// </summary>
+        /// <param name="first">The first <see cref="Team"/>.</param>
+        /// <param name="second">The second <see cref="Team"/>.</param>
+        public TwoTeams(Team first, Team second) : base(first, second) { }
+
+        /// <summary>
+        /// Gets the host <see cref="Team"/>.
+        /// </summary>
+        /// <value>
+        /// The host <see cref="Team"/>.
+        /// </value>
+        public Team GetHostTeam => this.Item1;
+        /// <summary>
+        /// Gets the guest <see cref="Team"/>.
+        /// </summary>
+        /// <value>
+        /// The guest <see cref="Team"/>.
+        /// </value>
+        public Team GetGuestTeam => this.Item2;
+    }
+}
