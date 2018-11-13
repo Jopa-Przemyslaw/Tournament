36f782a42c45b0b1de71f85e02aafda1022e1f4b
Jopa-Przemyslaw <przemyslaw.jopa@gmail.com>
2018-11-13 19:43:54 +0100

    Update Cup class.

--- Backend/Cup.cs
+++ Backend/Cup.cs
@@ -16,7 +16,27 @@ namespace Backend
         /// The list of teams in cup.
         /// </value>
         public List<Team> listOfTeamsInCup { get; private set; }
+        /// <summary>
+        /// Gets the list of <see cref="Referee"/>s in <see cref="Cup"/>.
+        /// </summary>
+        /// <value>
+        /// The list of <see cref="Referee"/>s in <see cref="Cup"/>.
+        /// </value>
         public List<Referee> listOfRefereesInCup { get; private set; }
+        /// <summary>
+        /// Gets the name of the <see cref="Cup"/>.
+        /// </summary>
+        /// <value>
+        /// The name of the <see cref="Cup"/>.
+        /// </value>
+        public string name { get; private set; }
+        /// <summary>
+        /// True if the Cup is already played out. False if it's not.
+        /// </summary>
+        /// <value>
+        ///   <c>true</c> if this instance is finished; otherwise, <c>false</c>.
+        /// </value>
+        public bool isFinished { get; private set; }
 
         private Random random;
         private MatchEngine matchEngine;
@@ -28,20 +48,38 @@ namespace Backend
         {
             listOfTeamsInCup = new List<Team>();
             listOfRefereesInCup = new List<Referee>();
+            name = "New Cup";
             random = new Random();
             matchEngine = new MatchEngine();
+            isFinished = false;
+        }
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Cup"/> class.
+        /// </summary>
+        /// <param name="name">The name of the new <see cref="Cup"/>.</param>
+        public Cup(string name)
+        {
+            listOfTeamsInCup = new List<Team>();
+            listOfRefereesInCup = new List<Referee>();
+            this.name = name;
+            random = new Random();
+            matchEngine = new MatchEngine();
+            isFinished = false;
         }
         /// <summary>
         /// Initializes a new instance of the <see cref="Cup" /> class.
         /// </summary>
-        /// <param name="listOfTeamsInCup">The list of <see cref="Team"/>s in <see cref="Cup"/>.</param>
-        /// <param name="listOfRefereesInCup">The list of <see cref="Referee"/>s in <see cref="Cup"/>.</param>
-        public Cup(List<Team> listOfTeamsInCup, List<Referee> listOfRefereesInCup)
+        /// <param name="listOfTeamsInCup">The list of <see cref="Team" />s in <see cref="Cup" />.</param>
+        /// <param name="listOfRefereesInCup">The list of <see cref="Referee" />s in <see cref="Cup" />.</param>
+        /// <param name="name">The name of the new <see cref="Cup"/>.</param>
+        public Cup(List<Team> listOfTeamsInCup, List<Referee> listOfRefereesInCup, string name)
         {
             this.listOfTeamsInCup = new List<Team>(listOfTeamsInCup);
             this.listOfRefereesInCup = new List<Referee>(listOfRefereesInCup);
+            this.name = name;
             random = new Random();
             matchEngine = new MatchEngine();
+            isFinished = false;
         }
 
         /// <summary>
@@ -50,7 +88,17 @@ namespace Backend
         /// <param name="team">The <see cref="Team"/> to add to a <see cref="Cup"/>.</param>
         public void AddTeam(Team team)
         {
-            listOfTeamsInCup.Add(team);
+            if (!listOfTeamsInCup.Contains(team))
+                listOfTeamsInCup.Add(team);
+        }
+        /// <summary>
+        /// Removes the <see cref="Team"/> from a <see cref="Cup"/> list.
+        /// </summary>
+        /// <param name="team">The <see cref="Team"/> to remove.</param>
+        public void RemoveTeam(Team team)
+        {
+            if (listOfTeamsInCup.Contains(team))
+                listOfTeamsInCup.Remove(team);
         }
         /// <summary>
         /// Returns the <see cref="Team"/> that takes part in the <see cref="Cup"/>.
@@ -67,7 +115,17 @@ namespace Backend
         /// <param name="referee">The <see cref="Referee"/> to add to a <see cref="Cup"/>.</param>
         public void AddReferee(Referee referee)
         {
-            listOfRefereesInCup.Add(referee);
+            if (!listOfRefereesInCup.Contains(referee))
+                listOfRefereesInCup.Add(referee);
+        }
+        /// <summary>
+        /// Removes the <see cref="Referee"/> from a <see cref="Cup"/> list.
+        /// </summary>
+        /// <param name="referee">The <see cref="Referee"/> to remove.</param>
+        public void RemoveReferee(Referee referee)
+        {
+            if (listOfRefereesInCup.Contains(referee))
+                listOfRefereesInCup.Remove(referee);
         }
         /// <summary>
         /// Returns the <see cref="Referee"/> that takes part in the <see cref="Cup"/>.
@@ -79,39 +137,65 @@ namespace Backend
             return listOfRefereesInCup.ElementAt(index);
         }
 
+        /// <summary>
+        /// Starts the football <see cref="Cup" />.
+        /// </summary>
+        /// <returns>
+        /// The winner <see cref="Team" />.
+        /// </returns>
+        /// <exception cref="Exception">Not enough teams or referees in a cup.
+        /// or
+        /// Not even number of teams in a cup.</exception>
         public Team StartFootballCup()
         {
+            if (isFinished)
+                throw new Exception("Cup already played out.");
             int teamsInCup = listOfTeamsInCup.Count();
             int refereesInCup = listOfRefereesInCup.Count();
             if (teamsInCup < 8 && refereesInCup < 3)
                 throw new Exception("Not enough teams or referees in a cup.");
             if (teamsInCup % 2 != 0)
+                throw new Exception("Not even number of teams in a cup.");
+            if (teamsInCup != 8 & teamsInCup != 16 & teamsInCup != 24)
+                throw new Exception("Need to be 8, 16 or 24 teams for a cup.");
+            int xx = 1;
+            int yy = listOfTeamsInCup.Count();
+            while ((yy / 2) > 1)
             {
-
+                xx++;
+                yy /= 2;
             }
-            for (int i = 0; i < teamsInCup - 1; i++)
+            for (int h = 0; h < xx; h++)
             {
-                TwoTeams teams = RandomTeamsForMatch();
-                Team teamA = teams.GetHostTeam;
-                Team teamB = teams.GetGuestTeam;
-
-                ThreeReferees referees = RandomRefereesForMatch();
-                Referee mainReferee = referees.GetMainReferee;
-                Referee suppRef1 = referees.GetSupportingReferee1;
-                Referee suppRef2 = referees.GetSupportingReferee2;
-
-                FootballMatch footballMatch = new FootballMatch(mainReferee, teamA, teamB, suppRef1, suppRef2);
-                matchEngine.SimulateFootballMatch(ref footballMatch, true);
-                if (footballMatch.GetScoreOfTeamA() > footballMatch.GetScoreOfTeamB())
+                List<Team> tempList = new List<Team>(listOfTeamsInCup);
+                for (int i = 0; i < teamsInCup / 2; i++)
                 {
-                    listOfTeamsInCup.Remove(teamB);
+                    TwoTeams teams = RandomTeamsForMatch(tempList);
+                    Team teamA = teams.GetHostTeam;
+                    Team teamB = teams.GetGuestTeam;
+
+                    ThreeReferees referees = RandomRefereesForMatch();
+                    Referee mainReferee = referees.GetMainReferee;
+                    Referee suppRef1 = referees.GetSupportingReferee1;
+                    Referee suppRef2 = referees.GetSupportingReferee2;
+
+                    FootballMatch footballMatch = new FootballMatch(mainReferee, teamA, teamB, suppRef1, suppRef2);
+                    matchEngine.SimulateFootballMatch(ref footballMatch, true);
+                    if (footballMatch.GetScoreOfTeamA() > footballMatch.GetScoreOfTeamB())
+                    {
+                        listOfTeamsInCup.Remove(teamB);
+                    }
+                    else if (footballMatch.GetScoreOfTeamB() > footballMatch.GetScoreOfTeamA())
+                    {
+                        listOfTeamsInCup.Remove(teamA);
+                    }
+                    tempList.Remove(teamA);
+                    tempList.Remove(teamB);
                 }
-                else if (footballMatch.GetScoreOfTeamB() > footballMatch.GetScoreOfTeamA())
-                {
-                    listOfTeamsInCup.Remove(teamA);
-                }
+                teamsInCup /= 2;
             }
-
+            listOfTeamsInCup.ElementAt(0).AddTournamentWon();
+            isFinished = true;
             return listOfTeamsInCup.ElementAt(0);
         }
 
@@ -119,7 +203,7 @@ namespace Backend
         /// Randoms two different <see cref="Team"/>s for the <see cref="Match"/> and returns them in object of <see cref="TwoTeams"/> type.
         /// </summary>
         /// <returns></returns>
-        private TwoTeams RandomTeamsForMatch()
+        private TwoTeams RandomTeamsForMatch(List<Team> listOfTeamsInCup)
         {
             List<Team> helpTeamList = new List<Team>(listOfTeamsInCup);
 
