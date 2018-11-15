99b9e2dcb3474b6dbc5853eed5c369676f8bd3fa
przemyslaw.jopa <przemyslaw.jopa@gmail.com>
2018-11-15 23:33:52 +0100

    Add Splash window.

---
+++ Tournament/Splash.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Tournament
+{
+    /// <summary>
+    /// Interaction logic for Splash.xaml
+    /// </summary>
+    public partial class Splash : Window
+    {
+        MainWindow mainWindow = new MainWindow();
+
+        public Splash()
+        {
+            InitializeComponent();
+        }
+
+        private void Move(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+
+        private void BtnClose_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void BtnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void BtnStartApp_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+            mainWindow.Show();
+        }
+    }
+}
